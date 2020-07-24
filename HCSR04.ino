e0077be449681a268505a11d57b1a9aa16e961df
Farrish Ravi <68719067+farrish09@users.noreply.github.com>
2020-07-24 10:07:45 +0700

    Add files via upload

---
+++ HCSR04.ino
@@ -0,0 +1,16 @@
+#include <HCSR04.h>
+
+#define trigger 5
+#define echo 4 
+
+HCSR04 distance (trigger , echo );
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  float jarak = distance.dist();
+  Serial.println(jarak);
+  delay(1000);
+}
